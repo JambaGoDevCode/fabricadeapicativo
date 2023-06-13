d176b707814d28448b3ed186412662c241f8e99e
João Jamba <jambapedrojoao@gmail.com>
2023-06-13 15:39:20 +0100

    feat: variables

---
+++ youDontKnowJS-chapter-2/variables.js
@@ -0,0 +1,14 @@
+/**
+ * In javascript, variable names (including function names) must be valid identifiers. 
+ * The strict and complete rules for valid characters in identifiers are a little complex 
+ * when you consider nontraditional characters such as unicode. if you only consider typical 
+ * ASCII alphanumeric characters, though, the rules are simple.
+ * 
+ * An identifier must start with a-z, A-Z, $, or _. It can then contain any of those
+ * characters plus the numerals 0-9.
+ * 
+ * Generally, the same rules apply to a property name as to a variable identifier.
+ * However, certain words cannot be used as variables, but are OK as property names.
+ * These words are called  "reserved words," and include the JS keywords (for, in, if, etc.)
+ * as well as null, true, and false.
+ */
