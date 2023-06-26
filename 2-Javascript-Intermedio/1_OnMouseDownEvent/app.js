a0625cd0bbee2adbb0f8d7beac167203f6d25937
João Jamba <jambapedrojoao@gmail.com>
2023-06-26 23:52:44 +0100

    event keybord

---
+++ 2-Javascript-Intermedio/1_OnMouseDownEvent/app.js
@@ -0,0 +1,28 @@
+
+function click(){
+    console.log('Apertou o mouse')
+}
+
+function leftClick(){
+    console.log('Soltou o mouse')
+}
+
+function passTopClick(){
+    console.log('passou por cima com mouse')
+}
+
+function LeftpassTopClick(){
+    console.log('tirou por cima com mouse')
+}
+
+function moveClick(){
+    console.log('moveu por cima com mouse')
+}
+
+function clicavel(){
+    console.log('Cliquei no botão')
+}
+
+function botaoDireito(){
+    console.log('Clicando com a direita')
+}
